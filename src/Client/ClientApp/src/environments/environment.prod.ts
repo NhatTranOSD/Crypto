69f44203fd5e9f667f9e72aca0a3d44bf7b38ab0
Nhat Tran <tranvannhatdev@gmail.com>
2019-06-15 08:35:07 +0700

    Change env

--- src/Client/ClientApp/src/environments/environment.prod.ts
+++ src/Client/ClientApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authApi: 'https://crypto-securityservice.azurewebsites.net',
-  walletApi: 'https://crypto-walletservice.azurewebsites.net',
-  shoppingApi: 'https://crypto-shoppingservice.azurewebsites.net',
+  authApi: 'https://crypto-securityservice.azurewebsites.net/',
+  walletApi: 'https://cryptowalletservice.azurewebsites.net/',
+  shoppingApi: 'https://crypto-shoppingservice.azurewebsites.net/',
 };
