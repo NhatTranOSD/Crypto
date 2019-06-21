7247b80bad4d43ead4de0767d0995644b57fa318
tranvannhatdev <tranvannhatdev@gmail.com>
2019-06-21 15:27:59 +0700

    Add db

---
+++ src/Server/Crypto/WalletService/Data/Entities/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalletService.Data.Entities
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+
+        public string Address { get; set; }
+
+        public string PrivateKey { get; set; }
+    }
+}
